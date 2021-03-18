5a16f1d178dbedbe07d432c15f0acc3f7767e5ea
Hauke Rehfeld <git@haukerehfeld.de>
2021-03-18 19:53:21 +0100

    chg: version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='keepassxc-browser',
-    version='0.1.5',
+    version='0.1.6',
     packages=find_packages(),
     install_requires=[
         'pysodium',
