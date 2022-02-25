hrehfeld/python-keepassxc-browser

commit 6fcb9b69fc184c6327c16dcb5e53b471ce285f5c

add: support windows sockets

keepassxc_browser/protocol.py
@@ -7,6 +7,13 @@ from pathlib import Path
 
 import pysodium
 
+if platform.system() == "Windows":
+    import win32file
+    from .connection_win import WinSock
+
+else:
+    from .connection_posix import DefaultSock
+
 from .exceptions import ProtocolError
 
 BUFF_SIZE = 1024 * 1024
@@ -109,44 +116,36 @@ class Connection:
             xdg_runtime_dir = Path(xdg_runtime_dir)
             runtime_socket_path = xdg_runtime_dir / socket_name
 
-        if platform.system() == "Darwin" and tmpdir and tmpdir_socket_path.exists():
+        if platform.system() == "Windows":
+            server_address = f"{DEFAULT_SOCKET_NAME}_{os.getenv('USERNAME')}"
+            sock = WinSock(win32file.GENERIC_READ | win32file.GENERIC_WRITE, win32file.OPEN_EXISTING)
+        elif platform.system() == "Darwin" and tmpdir and tmpdir_socket_path.exists():
             server_address = tmpdir_socket_path
+            sock = DefaultSock(DEFAULT_SOCKET_TIMEOUT, BUFF_SIZE)
         elif xdg_runtime_dir and runtime_socket_path.exists():
             server_address = runtime_socket_path
             # TODO: tmpdir is untested
+            sock = DefaultSock(DEFAULT_SOCKET_TIMEOUT, BUFF_SIZE)
         elif tmpdir and tmpdir_socket_path.exists():
             server_address = tmpdir_socket_path
+            sock = DefaultSock(DEFAULT_SOCKET_TIMEOUT, BUFF_SIZE)
         else:
             raise OSError('Unknown path for keepassxc socket.')
 
         self.server_address = server_address
-
-        self.sock = None
+        self.connection = sock
 
     def connect(self):
-        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        sock.settimeout(DEFAULT_SOCKET_TIMEOUT)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, BUFF_SIZE)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
-
-        try:
-            sock.connect(str(self.server_address))
-        except socket.error:
-            sock.close()
-            raise Exception(
-                "Could not connect to {addr}".format(addr=self.server_address)
-            )
-
-        self.sock = sock
+        self.connection.connect(str(self.server_address))
 
     def disconnect(self):
-        self.sock.close()
+        self.connection.close()
 
     def send(self, command):
         assert isinstance(command, str)
-        self.sock.send(command.encode())
+        self.connection.send(command.encode())
 
-        resp, server = self.sock.recvfrom(BUFF_SIZE)
+        resp, server = self.connection.recvfrom(BUFF_SIZE)
         r = resp.decode()
         return r
 
@@ -299,7 +298,7 @@ class Connection:
         """
         while True:
             try:
-                action = json.loads(self.sock.recv(BUFF_SIZE).decode())['action']
+                action = json.loads(self.connection.recv(BUFF_SIZE).decode())['action']
                 if action == "database-unlocked":
                     break
             except socket.timeout:

keepassxc_browser/connection_posix.py
@@ -1,0 +1,28 @@
+import socket
+
+
+class DefaultSock:
+
+    def __init__(self, timeout, buff_size):
+        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self.sock.settimeout(timeout)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, buff_size)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, buff_size)
+
+    def connect(self, address):
+        try:
+            self.sock.connect(address)
+        except socket.error:
+            self.sock.close()
+            raise Exception(
+                "Could not connect to {addr}".format(addr=address)
+            )
+
+    def close(self):
+        self.sock.close()
+
+    def send(self, message):
+        self.sock.send(message)
+
+    def recvfrom(self, buff_size):
+        return self.sock.recvfrom(buff_size)

keepassxc_browser/connection_win.py
@@ -1,0 +1,42 @@
+import win32file
+
+
+class WinSock:
+    """ A basic socket wrapper for Windows named pipes """
+
+    def __init__(self, desired_access, creation_disposition, share_mode=0,
+                 security_attributes=None, flags_and_attributes=0, input_nullok=None):
+        self.desired_access = desired_access
+        self.creation_disposition = creation_disposition
+        self.share_mode = share_mode
+        self.security_attributes = security_attributes
+        self.flags_and_attributes = flags_and_attributes
+        self.input_nullok = input_nullok
+        self.handle = None
+
+    def connect(self, address):
+        try:
+            self.handle = win32file.CreateFile(
+                r'\\.\pipe\%s' % address,
+                self.desired_access,
+                self.share_mode,
+                self.security_attributes,
+                self.creation_disposition,
+                self.flags_and_attributes,
+                self.input_nullok
+            )
+        except Exception as e:
+            raise Exception(
+                "Could not connect to pipe {addr}".format(addr=address), e
+            )
+
+    def close(self):
+        if self.handle:
+            self.handle.close()
+
+    def send(self, message):
+        win32file.WriteFile(self.handle, message)
+
+    def recvfrom(self, buff_size):
+        response_code, data = win32file.ReadFile(self.handle, buff_size)
+        return data, response_code

setup.py
@@ -4,7 +4,10 @@ setup(
     name='keepassxc-browser',
     version='0.1.5',
     packages=find_packages(),
-    install_requires=['pysodium'],
+    install_requires=[
+        'pysodium',
+        'pywin32; platform_system == "Windows"'
+    ],
     description='Access the KeepassXC Browser API from python',
     url='https://github.com/hrehfeld/python-keepassxc-browser',
     author='Hauke Rehfeld',

commit 32efd3c6fb3e78f711eea0560bddbc131ee6e184

Fix wait_for_unlock()
This change fixes an issue introduced by
6fcb9b69fc184c6327c16dcb5e53b471ce285f5c, where a connection is no
longer a socket but a wrapper. For example,

$ cat example2.py

from pathlib import Path

from keepassxc_browser import Connection, Identity, ProtocolError

client_id = 'python-keepassxc-browser'

state_file = Path('.assoc')
if state_file.exists():
    with state_file.open('r') as f:
        data = f.read()
    id = Identity.unserialize(client_id, data)
else:
    id = Identity(client_id)

c = Connection()
c.connect()
c.change_public_keys(id)
try:
    db_hash = c.get_database_hash(id)
except ProtocolError as ex:
    print(ex)
    c.wait_for_unlock()
print(db_hash)

c.disconnect()

$ python example2.py
資料庫未開啟
Traceback (most recent call last):
  File "/home/yen/var/local/Computer/keepassxc/python-keepassxc-browser/example2.py", line 19, in <module>
    db_hash = c.get_database_hash(id)
  File "/home/yen/var/local/Computer/keepassxc/python-keepassxc-browser/keepassxc_browser/protocol.py", line 189, in get_database_hash
    resp_message = self.encrypt_message_send_command(identity, action, message)
  File "/home/yen/var/local/Computer/keepassxc/python-keepassxc-browser/keepassxc_browser/protocol.py", line 172, in encrypt_message_send_command
    return self.send_encrypted_command(identity, command, nonce)
  File "/home/yen/var/local/Computer/keepassxc/python-keepassxc-browser/keepassxc_browser/protocol.py", line 166, in send_encrypted_command
    resp = self.send_command(identity, command, nonce, next_nonce)
  File "/home/yen/var/local/Computer/keepassxc/python-keepassxc-browser/keepassxc_browser/protocol.py", line 160, in send_command
    raise ProtocolError(resp['error'])
keepassxc_browser.exceptions.ProtocolError: 資料庫未開啟

During handling of the above exception, another exception occurred:

Traceback (most recent call last):
  File "/home/yen/var/local/Computer/keepassxc/python-keepassxc-browser/example2.py", line 22, in <module>

keepassxc_browser/protocol.py
@@ -298,7 +298,7 @@ class Connection:
         """
         while True:
             try:
-                action = json.loads(self.connection.recv(BUFF_SIZE).decode())['action']
+                action = json.loads(self.connection.recvfrom(BUFF_SIZE)[0].decode())['action']
                 if action == "database-unlocked":
                     break
             except socket.timeout: